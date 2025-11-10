87343cabb4f35a2527a51fb0e853c4385fec60f1
Juan Pablo Carrillo Hortua <juanpablocarrillohortua@gmail.com>
2025-11-10 18:32:06 -0500

    Src completo hasta el momento

--- src/tablas_cruzadas.py
+++ src/tablas_cruzadas.py
@@ -2,7 +2,7 @@ import pandas  as pd
 from typing import Optional, List, Dict, Any, Literal
 
 
-def cruzar_por_survived(data, col):
+def cruzar_por_survived(data, col): ## cruza segun una culumna especifica y la supervivencia
     df_grouped = data.groupby([col, 'Survived']).size().reset_index(name='Count')
     df_grouped['Supervivencia_Etiqueta'] = df_grouped['Survived'].apply(lambda x: 'Sí Sobrevivió' if x == 1 else 'No Sobrevivió')
     df_grouped['Total_by_col'] = df_grouped.groupby(col)['Count'].transform('sum') ##aplicar la suma del total de cada genero a las filas de ese genero
@@ -17,31 +17,74 @@ def clasificar_edades(edad):
     else:
         return 'ninguno de los grupos de interes'
 
-
 def cruzar_por_survived_etaria(data:pd.DataFrame, gender: Optional[Literal['male', 'female']] = None, categoria: Optional[Literal['k_m', 'c_d']] = None):
-    if gender:
+    #cruce con clacificacion etaria (categoria: k_m; kid vs man, c_d cincuenta vs diez)
+    #tambien da la posibilidad de discriminar elm genero
+
+    #discriminacion por genero
+    if gender: 
         try:
             df_grouped = data[data['Sex'] == gender].copy()
         except Exception as e:
             raise ValueError('genero no existe')
     else:
         df_grouped = data.copy()
-        
-    if categoria == "k_m": #kid man
+
+    if categoria == "k_m": #kid man, clacifique con criterio de mayoria de edad
         df_grouped['grupo etario'] = df_grouped['Age'].apply(lambda x: 'niño' if x < 18 else "hombre")
-    elif categoria == "c_d": #cincuenton 10
+
+    elif categoria == "c_d": #clacifique con criterio de edades 50, 10 u otro
         df_grouped['grupo etario'] = df_grouped['Age'].apply(clasificar_edades)
 
-    df_grouped = df_grouped.groupby(['grupo etario', 'Survived']).size().reset_index(name='Count')
-    df_grouped['Supervivencia_Etiqueta'] = df_grouped['Survived'].apply(lambda x: 'Sí Sobrevivió' if x == 1 else 'No Sobrevivió')
+    df_grouped = df_grouped.groupby(['grupo etario', 'Survived']).size().reset_index(name='Count') #conteo
+    df_grouped['Supervivencia_Etiqueta'] = df_grouped['Survived'].apply(lambda x: 'Sí Sobrevivió' if x == 1 else 'No Sobrevivió') #etiqueta de supervivencia
+
     df_grouped['total por grupo'] = df_grouped.groupby('grupo etario')['Count'].transform('sum')
     df_grouped['Percent'] = (df_grouped['Count']/df_grouped['total por grupo'])*100
+
+    #ajustar output
     if categoria == "k_m":
         df_hombre = df_grouped[df_grouped['grupo etario'] == 'hombre']
         df_nino = df_grouped[df_grouped['grupo etario'] == 'niño']
         return [df_grouped, df_hombre, df_nino]
+    
     else:
         df_cincuenta = df_grouped[df_grouped['grupo etario'] == 'mayor a 50 años']
         df_diez = df_grouped[df_grouped['grupo etario'] == 'menor a 10 años']
         df_otro = df_grouped[df_grouped['grupo etario'] == 'ninguno de los grupos de interes']
         return [df_grouped, df_cincuenta, df_diez, df_otro]
+    
+
+
+def cruzar_ports_survived(data:pd.DataFrame, port_interest: Optional[Literal['S', 'C', 'Q']] = None, only_surv=True,  only_interest_port = False):
+    #que puerto interesa
+    match port_interest:
+        case 'S':
+            puertos = {"S": "Southampton", "C": "Otro", "Q":"Otro"}
+        case 'C':
+            puertos = {"S": "Otro", "C": "Cherbourg", "Q":"Otro"}
+        case 'Q':
+            puertos = {"S": "Otro", "C": "Otro", "Q":"Queenstown"}
+        case _:
+            puertos = {"S": "Southampton", "C": "Cherbourg", "Q":"Queenstown"}
+    df_grouped = data.copy()
+
+    #labeling segun interes
+    df_grouped['Puerto de interes'] = df_grouped['Embarked'].map(puertos)
+
+    df_grouped = df_grouped.groupby(['Puerto de interes', 'Survived']).size().reset_index(name="Conteo")
+    df_grouped["Total puerto"] = df_grouped.groupby('Puerto de interes')["Conteo"].transform('sum')
+    df_grouped['Percent'] = (df_grouped['Conteo']/df_grouped['Total puerto'])*100
+    df_grouped['Supervivencia_Etiqueta'] = df_grouped['Survived'].apply(lambda x: 'Sí Sobrevivió' if x == 1 else 'No Sobrevivió')
+
+    #ajustar output 
+    #only_surv: solo muestra survived ==1
+    #only_interest_port: solo muestra port == port_interest
+    if only_surv:
+        return df_grouped[df_grouped['Survived'] == 1]
+    elif only_interest_port:
+        return df_grouped[df_grouped['Puerto de interes'] == puertos[port_interest]]
+    elif only_surv and only_interest_port:
+        return df_grouped[(df_grouped['Puerto de interes'] == puertos[port_interest]) & (df_grouped['Survived'] == 1)]
+    else:
+        return df_grouped
