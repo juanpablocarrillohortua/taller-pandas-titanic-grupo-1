91d8ed60dcc3184acd7fba13538626d4b72ba0f8
Juan Pablo Carrillo Hortua <juanpablocarrillohortua@gmail.com>
2025-11-10 20:48:26 -0500

    version final tablas cruzadas (funciones actuales)

--- src/tablas_cruzadas.py
+++ src/tablas_cruzadas.py
@@ -9,6 +9,9 @@ def cruzar_por_survived(data, col): ## cruza segun una culumna especifica y la s
     df_grouped['Percentage'] = (df_grouped['Count'] / df_grouped['Total_by_col']) * 100 
     return df_grouped
 
+
+
+
 def clasificar_edades(edad):
     if edad < 10:
         return "menor a 10 años"
@@ -16,10 +19,27 @@ def clasificar_edades(edad):
         return "mayor a 50 años"
     else:
         return 'ninguno de los grupos de interes'
+    
+def clasificar_edades_estandar(edad):
+    if edad < 10:
+        return 'Niño'
+    elif 10 <= edad < 18:
+        return 'Adolescente' 
+    elif 18 <= edad <= 49:
+        return 'Adulto'
+    else:
+        return 'Mayor'
+    
+
 
 def cruzar_por_survived_etaria(data:pd.DataFrame, gender: Optional[Literal['male', 'female']] = None, categoria: Optional[Literal['k_m', 'c_d']] = None):
-    #cruce con clacificacion etaria (categoria: k_m; kid vs man, c_d cincuenta vs diez)
-    #tambien da la posibilidad de discriminar elm genero
+    #cruce con clacificacion etaria 
+    # categorias: 
+
+    # k_m; kid vs man
+    # c_d cincuenta vs diez
+
+    #tambien da la posibilidad de discriminar el genero
 
     #discriminacion por genero
     if gender: 
@@ -35,10 +55,17 @@ def cruzar_por_survived_etaria(data:pd.DataFrame, gender: Optional[Literal['male
 
     elif categoria == "c_d": #clacifique con criterio de edades 50, 10 u otro
         df_grouped['grupo etario'] = df_grouped['Age'].apply(clasificar_edades)
+    
 
+
+
+    #arupamiento
     df_grouped = df_grouped.groupby(['grupo etario', 'Survived']).size().reset_index(name='Count') #conteo
+
+
     df_grouped['Supervivencia_Etiqueta'] = df_grouped['Survived'].apply(lambda x: 'Sí Sobrevivió' if x == 1 else 'No Sobrevivió') #etiqueta de supervivencia
 
+    #porcentajes
     df_grouped['total por grupo'] = df_grouped.groupby('grupo etario')['Count'].transform('sum')
     df_grouped['Percent'] = (df_grouped['Count']/df_grouped['total por grupo'])*100
 
@@ -48,12 +75,16 @@ def cruzar_por_survived_etaria(data:pd.DataFrame, gender: Optional[Literal['male
         df_nino = df_grouped[df_grouped['grupo etario'] == 'niño']
         return [df_grouped, df_hombre, df_nino]
     
-    else:
+    elif categoria == 'c_d':
         df_cincuenta = df_grouped[df_grouped['grupo etario'] == 'mayor a 50 años']
         df_diez = df_grouped[df_grouped['grupo etario'] == 'menor a 10 años']
         df_otro = df_grouped[df_grouped['grupo etario'] == 'ninguno de los grupos de interes']
         return [df_grouped, df_cincuenta, df_diez, df_otro]
     
+    
+
+
+
 
 
 def cruzar_ports_survived(data:pd.DataFrame, port_interest: Optional[Literal['S', 'C', 'Q']] = None, only_surv=True,  only_interest_port = False):
@@ -89,6 +120,8 @@ def cruzar_ports_survived(data:pd.DataFrame, port_interest: Optional[Literal['S'
     else:
         return df_grouped
     
+
+
 
 
 def segun_cabina(data):
